74a3cec0c46ad3b50618f90c92db558ce7bb5523
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-10-16 20:47:44 +0300

    no issue: vuetify folder moved in src

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
+
+const opts = {};
+// primary: #66FCF1
+// secondary: #C3073F
+export default new Vuetify(opts);
